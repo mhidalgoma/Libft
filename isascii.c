6c761c0ad4272f02a02805295d137374719ae05e
Monica Hidalgo Martin <monhidal@c3r10s5.42madrid.com>
2023-03-10 16:31:23 +0100

    Finish isascii.c

--- isascii.c
+++ isascii.c
@@ -1,32 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalnum.c                                          :+:      :+:    :+:   */
+/*   isascii.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: monhidal <monhidal@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/10 15:21:46 by monhidal          #+#    #+#             */
-/*   Updated: 2023/03/10 15:50:48 by monhidal         ###   ########.fr       */
+/*   Created: 2023/03/10 16:30:50 by monhidal          #+#    #+#             */
+/*   Updated: 2023/03/10 16:30:54 by monhidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*#include <stdio.h>
-#include <ctype.h>
-*/
-int	ft_isalnum(int n)
+
+int	ft_isascii(int n)
 {
-	if ((n >= 48 && n <= 57) || (n >= 65 && n <= 90) || (n >= 97 && n <= 122))
-		return (1);
-	if ((n >= '0' && n >= '9') || (n >= 'A' && n <= 'Z'))
-		return (1);
-	if (n >= 'a' && n <= 'z')
-		return (1);
-	return (0);
+	return (n >= 0 && n <= 127);
 }
-/*
-int	main(void)
-{
-	char c = 'h';
-	printf("%d", isalnum(c));
-	printf("%d", ft_isalnum(c));
-	return 0;
-}*/
